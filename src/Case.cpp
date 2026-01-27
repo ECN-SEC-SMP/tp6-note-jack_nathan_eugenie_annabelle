9484bcba06fc145d90b46691c39bd088a00ab332
Thomas Verron <verronthomas03@gmail.com>
2026-01-27 16:30:08 +0100

    Debug Board + Case / Add tests

--- src/Case.cpp
+++ src/Case.cpp
@@ -1,8 +1,9 @@
 #include "Case.hpp"
 
-Case::Case()
- : x(0), y(0)
-{}
+Case::Case() : x(0), y(0)
+{
+    for (int i = 0; i < 3; i++) pion[i] = nullptr;
+}
 
 Case::Case(int x, int y)
     : x(x) , y(y)
@@ -24,49 +25,36 @@ Case::~Case()
     }
 }
 
-void Case::addPion(Pion* p){
-    switch (p->taille)
-    {
-    case SMALL:
-        pion[0] = p;
-        break;
-    case MEDIUM:
-        pion[1] = p;
-        break;
-    case LARGE:
-        pion[2] = p;
-        break;
-    default:
-        break;
+void Case::addPion(Pion* p)
+{
+    int idx = -1;
+    if (p->taille == SMALL) idx = 0;
+    else if (p->taille == MEDIUM) idx = 1;
+    else if (p->taille == LARGE) idx = 2;
+
+    if (idx != -1) {
+        delete pion[idx];     
+        pion[idx] = p;
     }
 }
 
 void Case::deletePion(SIZE s)
 {
-    for(int i = 0; i < 3; i++) {
-        if(pion[i]->taille == s)
-        {
+    for (int i = 0; i < 3; i++) {
+        if (pion[i] != nullptr && pion[i]->taille == s) {
             delete pion[i];
+            pion[i] = nullptr;
         }
     }
 }
 
-Pion Case::getPion(SIZE s)
+
+Pion* Case::getPion(SIZE s)
 {
-    switch (p->taille)
-    {
-    case SMALL:
-        return pion[0];
-        break;
-    case MEDIUM:
-        return pion[1];
-        break;
-    case LARGE:
-        return pion[2];
-        break;
-    default:
-        break;
-    }
+    if (s == SMALL)  return pion[0];
+    if (s == MEDIUM) return pion[1];
+    if (s == LARGE)  return pion[2];
+    return nullptr;
 }
 
 
