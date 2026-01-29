2cdf72fc4f796127a65e44a6f640357fa6f7d0cf
Roquain <e.roquain@yahoo.com>
2026-01-29 17:42:24 +0100

    Classe Human

---
+++ src/Human.cpp
@@ -0,0 +1,67 @@
+/**
+ * @file Human.cpp
+ * @brief 
+ */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "Human.hpp"
+
+playerAction_t playTurn(Board* board, cursor_t* cursor)
+    {
+        static cursor_t cursor = {0};
+        InputKey_t key;
+        Input* input = Input::GetInstance();
+        key = input->waitCtrlKey();
+        
+        // Déplacements
+        if (ARROW_UP == key) {
+            if (cursor->x < 2) {
+                cursor->x++;
+            }
+            return ARROW;
+        }
+        if (ARROW_DOWN == key) {
+            if (cursor->x > 0) {
+                cursor->x--;
+            }
+            return ARROW;
+        }
+        if (ARROW_RIGHT == key) {
+            if (cursor->y < 2) {
+                cursor->y++;
+            }
+            return ARROW;
+        }
+        if (ARROW_LEFT == key) {
+            if (cursor->y > 0) {
+                cursor->y--;
+            }
+            return ARROW;
+        }
+
+        if(key == KEY_TAB) {
+            return PION_SEL_ENABLE
+        }
+
+        if(key == KEY_ENTER){
+            COLOR c = this->color[0]; //Première couleur du joueur
+            SIZE s = SIZE::MEDIUM; //Par défaut taille Medium
+            
+            if (!hasPion(c, s)) {
+                return PION_SEL_DISABLE;
+            }
+            Case& selectedCase = board->getCase(cursor->x, cursor->y);
+        
+            if (selectedCase.getPion(s) == nullptr) {
+                Pion* newPion = new Pion{c, s};
+                selectedCase.addPion(newPion);
+                usePion(c, s);
+                
+                return ENDPLAY;
+            }
+        }
+
+        return ARROW;
+    }
